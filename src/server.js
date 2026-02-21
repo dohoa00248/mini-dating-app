ec202ac00ca52ae49f6a0f46cece52f5b9089230
dohoa00248 <dohoa00248@gmail.com>
2026-02-21 18:53:46 +0700

    chore: separate express app configuration from server listener

--- src/server.js
+++ src/server.js
@@ -1,16 +1,11 @@
-import express from 'express';
+import app from './app.js';
 import dotenv from 'dotenv';
 import connectDB from './config/db.js';
 
 dotenv.config();
-const app = express();
 
 const port = process.env.PORT;
 await connectDB();
-
-app.get('/', (req, res) => {
-  res.send('Hello world');
-});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
