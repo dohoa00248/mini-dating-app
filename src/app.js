dc7173b4ea0ed3061b36572c05bd518388459a52
dohoa00248 <dohoa00248@gmail.com>
2026-02-21 19:19:58 +0700

    refactor: move route definitions to dedicated router files

--- src/app.js
+++ src/app.js
@@ -1,9 +1,8 @@
+import indexRouter from './routes/index.js';
 import express from 'express';
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello world');
-});
+app.use('/', indexRouter);
 
 export default app;
