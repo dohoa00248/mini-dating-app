1039496964e030303692cab051846ef7156dca90
dohoa00248 <dohoa00248@gmail.com>
2026-02-26 19:14:49 +0700

    feat: set up home views

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,7 @@ import express from 'express';
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.send('Hello world');
+  res.render('home.ejs');
 });
 
 export default router;
